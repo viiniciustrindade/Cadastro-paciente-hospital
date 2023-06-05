1f90dc4959d157c7b94f01d742ad067e64dd5873
Vinicius Trindade <119625027+viiniciustrindade@users.noreply.github.com>
2023-06-05 11:28:17 -0300

    Inicialização do layout do formulario.

---
+++ Cadastro-paciente-hospital/FrmCadastroPaciente.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Cadastro_paciente_hospital
+{
+    partial class FrmCadastroPaciente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.cbxSexo = new System.Windows.Forms.ComboBox();
+            this.dtpDataNasc = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblExibirIdade = new System.Windows.Forms.Label();
+            this.txtNomePaciente = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mskCpf = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mskRg = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtMãe = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sexo";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(58, 12);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(58, 20);
+            this.txtCodigo.TabIndex = 2;
+            // 
+            // cbxSexo
+            // 
+            this.cbxSexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxSexo.FormattingEnabled = true;
+            this.cbxSexo.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbxSexo.Location = new System.Drawing.Point(159, 12);
+            this.cbxSexo.Name = "cbxSexo";
+            this.cbxSexo.Size = new System.Drawing.Size(42, 21);
+            this.cbxSexo.TabIndex = 3;
+            // 
+            // dtpDataNasc
+            // 
+            this.dtpDataNasc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDataNasc.Location = new System.Drawing.Point(317, 12);
+            this.dtpDataNasc.Name = "dtpDataNasc";
+            this.dtpDataNasc.Size = new System.Drawing.Size(99, 20);
+            this.dtpDataNasc.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data de nascimento";
+            // 
+            // lblExibirIdade
+            // 
+            this.lblExibirIdade.AutoSize = true;
+            this.lblExibirIdade.Location = new System.Drawing.Point(422, 15);
+            this.lblExibirIdade.Name = "lblExibirIdade";
+            this.lblExibirIdade.Size = new System.Drawing.Size(45, 13);
+            this.lblExibirIdade.TabIndex = 6;
+            this.lblExibirIdade.Text = "23 anos";
+            // 
+            // txtNomePaciente
+            // 
+            this.txtNomePaciente.Enabled = false;
+            this.txtNomePaciente.Location = new System.Drawing.Point(58, 38);
+            this.txtNomePaciente.Name = "txtNomePaciente";
+            this.txtNomePaciente.Size = new System.Drawing.Size(253, 20);
+            this.txtNomePaciente.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nome";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(317, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Cpf";
+            // 
+            // mskCpf
+            // 
+            this.mskCpf.Location = new System.Drawing.Point(346, 38);
+            this.mskCpf.Mask = "000,000,000-00";
+            this.mskCpf.Name = "mskCpf";
+            this.mskCpf.Size = new System.Drawing.Size(104, 20);
+            this.mskCpf.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(456, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Rg";
+            // 
+            // mskRg
+            // 
+            this.mskRg.Location = new System.Drawing.Point(483, 38);
+            this.mskRg.Mask = "00,000,000-0";
+            this.mskRg.Name = "mskRg";
+            this.mskRg.Size = new System.Drawing.Size(81, 20);
+            this.mskRg.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Mãe";
+            // 
+            // txtMãe
+            // 
+            this.txtMãe.Enabled = false;
+            this.txtMãe.Location = new System.Drawing.Point(58, 64);
+            this.txtMãe.Name = "txtMãe";
+            this.txtMãe.Size = new System.Drawing.Size(253, 20);
+            this.txtMãe.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 67);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Cor";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Branco",
+            "Preto",
+            "Pardo",
+            "Amarelo",
+            "Indígena"});
+            this.comboBox1.Location = new System.Drawing.Point(346, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // FrmCadastroPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 419);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtMãe);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.mskRg);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mskCpf);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNomePaciente);
+            this.Controls.Add(this.lblExibirIdade);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpDataNasc);
+            this.Controls.Add(this.cbxSexo);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCadastroPaciente";
+            this.Text = "Cadastro paciente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ComboBox cbxSexo;
+        private System.Windows.Forms.DateTimePicker dtpDataNasc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblExibirIdade;
+        private System.Windows.Forms.TextBox txtNomePaciente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mskCpf;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox mskRg;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtMãe;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
